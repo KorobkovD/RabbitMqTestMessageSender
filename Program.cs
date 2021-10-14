de94acadc2af5ba0bb3e2293e978311284a4c21c
KorobkovD <den-kor.int@mail.ru>
2021-10-14 12:02:11 +0300

    Added MassTransit implementation of messages handling

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 using Domain.Entities.Payler;
-using EasyNetQ;
-using EasyNetQ.Topology;
-using JsonSerializer = System.Text.Json.JsonSerializer;
+using MassTransit;
 
 namespace RabbitMqTestMessageSender
 {
@@ -40,48 +41,102 @@ namespace RabbitMqTestMessageSender
             };
         }
 
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            var rabbitUrl = "localhost";
+
             Console.WriteLine("RabbitMQ bus preparation...");
 
-            using (var bus = RabbitHutch.CreateBus("host=localhost;username=guest;password=guest").Advanced)
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
-                var exchange = bus.ExchangeDeclare(QueueName, ExchangeType.Topic);
-                var queue = bus.QueueDeclare(QueueName);
-                bus.Bind(exchange, queue, QueueName);
-                Console.WriteLine($"Press '+' to publish random {nameof(YandexNotification)} message,");
-                Console.Write($"press 'u' to publish {nameof(SomeUnexpectedMessage)} to the RabbitMQ: ");
-                var key = Console.ReadLine();
-                while (key is "+" or "u")
+                cfg.Host(rabbitUrl, configurator =>
                 {
-                    Console.Clear();
-                    switch (key)
-                    {
-                        case "+":
-                            var message = MakeMessage();
-                            Console.WriteLine("Generated message:");
-                            Console.WriteLine(JsonSerializer.Serialize(message));
-                            Console.WriteLine("Publishing message to the query...");
-                            bus.Publish(exchange, QueueName, true, new Message<YandexNotification>(message));
-                            break;
-                        case "u":
-                            var unexpectedMessage = MakeUnexpectedMessage(); 
-                            Console.WriteLine("Generated unexpected message:");
-                            Console.WriteLine(JsonSerializer.Serialize(unexpectedMessage));
-                            Console.WriteLine("Publishing message to the query...");
-                            bus.Publish(exchange, QueueName, true, new Message<SomeUnexpectedMessage>(unexpectedMessage));
-                            break;
-                    }
+                    configurator.Username("guest");
+                    configurator.Password("guest");
+                });
+                // cfg.Publish<YandexNotification>(conf =>
+                // {
+                //     conf.Durable = false;
+                //     // conf.ExchangeType = "fanout";
+                // });
+            });
+            // var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
 
-                    Console.Write("Publish another message? ");
-                    key = Console.ReadLine();
+            await busControl.StartAsync(/*source.Token*/);
+            Console.WriteLine($"Press '+' to publish random {nameof(YandexNotification)} message,");
+            Console.Write($"press 'u' to publish {nameof(SomeUnexpectedMessage)} to the RabbitMQ: ");
+            // var endpoint = await busControl.GetSendEndpoint(new Uri($"queue:{QueueName}"));
+            var key = Console.ReadLine();
+
+            while (key is "+" or "u")
+            {
+                Console.Clear();
+                switch (key)
+                {
+                    case "+":
+                        var message = MakeMessage();
+                        Console.WriteLine("Generated message:");
+                        Console.WriteLine(JsonSerializer.Serialize(message));
+                        Console.WriteLine("Publishing message to the query...");
+                        // await endpoint.Send(message/*, source.Token*/);
+                        await busControl.Publish(message);
+                        break;
+                    case "u":
+                        var unexpectedMessage = MakeUnexpectedMessage();
+                        Console.WriteLine("Generated unexpected message:");
+                        Console.WriteLine(JsonSerializer.Serialize(unexpectedMessage));
+                        Console.WriteLine("Publishing message to the query...");
+                        // await endpoint.Send(unexpectedMessage/*, source.Token*/);
+                        await busControl.Publish(unexpectedMessage);
+                        break;
                 }
 
-                Console.WriteLine("Disposing bus...");
+                Console.Write("Publish another message? ");
+                key = Console.ReadLine();
             }
 
-            // Console.WriteLine("Disposing bus...");
+            Console.WriteLine("Disposing bus...");
+            await busControl.StopAsync(/*source.Token*/);
             Console.WriteLine("Application finished");
+
+            // using (var bus = RabbitHutch.CreateBus("host=localhost;username=guest;password=guest").Advanced)
+            // {
+            //     var exchange = bus.ExchangeDeclare(QueueName, ExchangeType.Topic);
+            //     var queue = bus.QueueDeclare(QueueName);
+            //     bus.Bind(exchange, queue, QueueName);
+            //     Console.WriteLine($"Press '+' to publish random {nameof(YandexNotification)} message,");
+            //     Console.Write($"press 'u' to publish {nameof(SomeUnexpectedMessage)} to the RabbitMQ: ");
+            //     var key = Console.ReadLine();
+            //     while (key is "+" or "u")
+            //     {
+            //         Console.Clear();
+            //         switch (key)
+            //         {
+            //             case "+":
+            //                 var message = MakeMessage();
+            //                 Console.WriteLine("Generated message:");
+            //                 Console.WriteLine(JsonSerializer.Serialize(message));
+            //                 Console.WriteLine("Publishing message to the query...");
+            //                 bus.Publish(exchange, QueueName, true, new Message<YandexNotification>(message));
+            //                 break;
+            //             case "u":
+            //                 var unexpectedMessage = MakeUnexpectedMessage(); 
+            //                 Console.WriteLine("Generated unexpected message:");
+            //                 Console.WriteLine(JsonSerializer.Serialize(unexpectedMessage));
+            //                 Console.WriteLine("Publishing message to the query...");
+            //                 bus.Publish(exchange, QueueName, true, new Message<SomeUnexpectedMessage>(unexpectedMessage));
+            //                 break;
+            //         }
+            //
+            //         Console.Write("Publish another message? ");
+            //         key = Console.ReadLine();
+            //     }
+            //
+            //     Console.WriteLine("Disposing bus...");
+            // }
+
+            // Console.WriteLine("Disposing bus...");
+            // Console.WriteLine("Application finished");
         }
     }
 }
